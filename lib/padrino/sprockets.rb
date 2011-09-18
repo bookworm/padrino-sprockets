c5891ff9382744e89a9460c01910b860f0c3dee2
Ken Erickson <bookworm.productions@gmail.com>
2011-09-18 09:36:07 -0700

    Re-factored and restructured.
    Basically I did was make options work with a block so you can do
    
    ```ruby
    assets do
      append_path 'assets/js'
      url = 'assets'
    end
    ```
    
    etc

--- lib/padrino/sprockets.rb
+++ lib/padrino/sprockets.rb
@@ -2,43 +2,21 @@
 require "sprockets/environment"
 module Padrino
   module Sprockets
-    class << self
-      def registered(app)
-        app.helpers Helpers
-      end
-    end
-    module Helpers
-      module ClassMethods
-        def sprockets(options={})
-          url   = options[:url] || 'assets'
-          _root = options[:root] || root
-          set :sprockets_url, url
-          use Padrino::Sprockets::App,:root => _root,:url => url
-        end
-      end
-      def self.included(base)
-        base.extend         ClassMethods
-      end
-    end #Helpers
-    class App
-      def initialize(app, options={})
-        @app = app
-        @root = options[:root]
-        url   =  options[:url] || 'assets'
-        @matcher = /^\/#{url}\/*/
-        @environment = ::Sprockets::Environment.new(@root) do |env|
-        end
-        @environment.append_path 'assets/javascripts'
-        @environment.append_path 'assets/stylesheets'
-        @environment.append_path 'assets/images'
-      end
-      def call(env)
-        path = env["PATH_INFO"]
-        logger.debug { "path:#{path} url:#{@url}" }
-        return @app.call(env) unless @matcher =~ path
-        env['PATH_INFO'].sub!(@matcher,'')
-        @environment.call(env)
-      end
-    end
-  end #Sprockets
-end #Padrino
+    def self.registered(app)
+      unless app.root?
+        raise Error, "Please set :root in your app."
+      end    
+      app.extend ClassMethods
+    end  
+      
+    PREFIX = File.dirname(__FILE__)
+
+    autoload :ClassMethods,  "#{PREFIX}/sprockets/class_methods"
+    autoload :Options,       "#{PREFIX}/sprockets/options"
+
+    Error = Class.new(StandardError)
+
+    require "#{PREFIX}/sprockets/version" 
+    require "#{PREFIX}/sprockets/app"
+  end
+end 
