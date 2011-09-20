7657166b71086a9eda8e4c4ef1b328e31319956b
Ken Erickson <bookworm.productions@gmail.com>
2011-09-20 13:19:26 -0700

    added back in the path info. otherwise returns 404, so thats what htat does.

--- lib/padrino/sprockets/app.rb
+++ lib/padrino/sprockets/app.rb
@@ -9,7 +9,8 @@ module Padrino
         path = env["PATH_INFO"]      
         logger.debug { "path:#{path} url:#{@options.url}" }
         return @app.call(env) unless (@options.stylesheets_matcher =~ path && @options.handle_stylesheets) || (@options.javascripts_matcher =~ path && @options.handle_javascripts)
-        # env['PATH_INFO'].sub!(@options.matcher,'')
+        env['PATH_INFO'].sub!(@options.javascripts_matcher,'') if @options.javascripts_matcher  =~ path 
+        env['PATH_INFO'].sub!(@options.stylesheets_matcher,'') if @options.stylesheets_matcher  =~ path
         @options.sprockets_env.call(env)
       end
     end
